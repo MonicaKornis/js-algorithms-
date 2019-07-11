1d8449a93ee5e8e761263c7cec9088cf06b5ddfa
monicakornis <2011mkornis@gmail.com>
2019-07-10 20:40:14 -0400

    adding static array notes

--- sortedArrayToBST.js
+++ sortedArrayToBST.js
@@ -7,9 +7,16 @@ class Node {
 }
 
 function sortedArrayToBST(array) {
-  if(array.length < 1) return array; 
+  if(array.length < 1) return array;
   let center = Math.round((array.length-1)/2);
   let root = new Node(array[center]);
 
+  let left = array.slice(0,center);
+  let right = array.slice(center+1);
+
+  if(left.length > 0) root.left = sortedArrayToBST(left);
+  if(right.length > 0) root.right = sortedArrayToBST(right);
+
+  return root; 
 
 }
